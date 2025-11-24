293c28bf832ef4fe744e3c03cf5942db9a9fb809
tb59 <thomas.bourdeaudhuy@gmail.com>
2025-11-24 14:27:30 +0100

    init correction TNE V1

--- api-root.js
+++ api-root.js
@@ -1,6 +1,6 @@
 roots = [
 	{	root:"http://tomnab.fr/paragraphes-api/api/",
-		hash:"aaf676719a146679fae26efe7714e43f",
+		hash:"ccc3164a48658d4d1ac8b63ef5eb249f",
 		pseudo:"tom"}, 
 	{	root:"http://localhost/api-paragraphes/api/",
 		hash:"10bca641466d835d3db9be02ab6e1d08",
