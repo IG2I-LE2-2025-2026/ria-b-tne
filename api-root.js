86959074b5f5e1da994f634787421412f12a7049
tb59 <thomas.bourdeaudhuy@gmail.com>
2025-11-24 12:58:42 +0100

    init correction TNE groupe B

---
+++ api-root.js
@@ -0,0 +1,13 @@
+roots = [
+	{	root:"http://tomnab.fr/paragraphes-api/api/",
+		hash:"aaf676719a146679fae26efe7714e43f",
+		pseudo:"tom"}, 
+	{	root:"http://localhost/api-paragraphes/api/",
+		hash:"10bca641466d835d3db9be02ab6e1d08",
+		pseudo:"tom"},
+	{	root:"http://localhost/api-paragraphes/api/",
+		hash:"b9edda3aacebbf26bdfb708540070c05",
+		pseudo:"isa"},
+]; 
+						
+console.log("Chargement fichier api-roots.js OK");				
